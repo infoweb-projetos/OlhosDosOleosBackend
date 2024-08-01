3146486082411c8db5844ace47c7bed96340412e
Th14g0o <geekland135@gmail.com>
2024-08-01 20:12:25 -0300

    puxando valores dos selects de cadastro das tabelas do supabase

--- scriptsSQL/script0001_CreatesTable.sql
+++ scriptsSQL/script0001_CreatesTable.sql
@@ -15,6 +15,24 @@ CREATE TABLE Usuario(
     Twitter VARCHAR NULL,
     Foto VARCHAR NULL
 );
+--PostGREE
+create table
+  public.usuario (
+    usuarioid serial not null,
+    nome character varying(200) not null,
+    tipo character varying null,
+    localizacao character varying(200) null,
+    descricao character varying(2000) null,
+    zap character varying null,
+    insta character varying null,
+    face character varying null,
+    twitter character varying null,
+    foto character varying null,
+    "Senha" character varying not null,
+    "Email" character varying not null,
+    constraint usuario_pkey primary key (usuarioid),
+    constraint usuario_Email_key unique ("Email")
+  ) tablespace pg_default;
 
 --Create table Categoria
 CREATE TABLE Categoria(
@@ -57,3 +75,156 @@ CREATE TABLE Mensagem(
     FOREIGN KEY (UsuarioDestinoId) REFERENCES Usuario(UsuarioId),
     FOREIGN KEY (ObraId) REFERENCES Obra(ObraId)
 );
+
+
+--enums/select
+create table  public.pais (
+  nome character varying(200) not null,
+  constraint pais_pkey primary key (nome)
+);
+INSERT INTO public.pais (nome) VALUES ('Brasil');
+
+CREATE TABLE public.estado (
+    nome character varying(200) not null,
+    CONSTRAINT estado_pkey PRIMARY KEY (nome)
+);
+INSERT INTO public.estado (nome) VALUES
+('AC'),  -- Acre
+('AL'),  -- Alagoas
+('AM'),  -- Amazonas
+('BA'),  -- Bahia
+('CE'),  -- Ceará
+('ES'),  -- Espírito Santo
+('GO'),  -- Goiás
+('MA'),  -- Maranhão
+('MT'),  -- Mato Grosso
+('MS'),  -- Mato Grosso do Sul
+('MG'),  -- Minas Gerais
+('PA'),  -- Pará
+('PB'),  -- Paraíba
+('PR'),  -- Paraná
+('PE'),  -- Pernambuco
+('PI'),  -- Piauí
+('RJ'),  -- Rio de Janeiro
+('RN'),  -- Rio Grande do Norte
+('RS'),  -- Rio Grande do Sul
+('RO'),  -- Rondônia
+('RR'),  -- Roraima
+('SC'),  -- Santa Catarina
+('SP'),  -- São Paulo
+('SE'),  -- Sergipe
+('TO'),  -- Tocantins
+('DF');  -- Distrito Federal
+
+CREATE TABLE public.cidade (
+    nome character varying(200) not null,
+    CONSTRAINT cidade_pkey PRIMARY KEY (nome)
+);
+INSERT INTO public.cidade (nome) VALUES
+('São Paulo'),
+('Rio de Janeiro'),
+('Salvador'),
+('Fortaleza'),
+('Belo Horizonte'),
+('Brasília'),
+('Manaus'),
+('Curitiba'),
+('Recife'),
+('Porto Alegre'),
+('Belém'),
+('Goiânia'),
+('São Luís'),
+('Maceió'),
+('Natal'),
+('João Pessoa'),
+('Teresina'),
+('Campo Grande'),
+('São Bernardo do Campo'),
+('São José dos Campos'),
+('Uberlândia'),
+('Sorocaba'),
+('Contagem'),
+('Aracaju'),
+('Feira de Santana'),
+('Joinville'),
+('Jaboatão dos Guararapes'),
+('Londrina'),
+('Niterói'),
+('Santos'),
+('Maua'),
+('Diadema'),
+('Canoas'),
+('Blumenau'),
+('Pelotas'),
+('Vila Velha'),
+('Nova Iguaçu'),
+('Volta Redonda'),
+('Carapicuíba'),
+('Guarulhos'),
+('São José do Rio Preto'),
+('Itajaí'),
+('Presidente Prudente'),
+('Ponta Grossa'),
+('Cuiabá'),
+('Campo Largo'),
+('Campos dos Goytacazes'),
+('Taubaté'),
+('Caxias do Sul'),
+('São Carlos'),
+('Lajeado'),
+('Santa Maria'),
+('Araxá'),
+('Ribeirão Preto'),
+('Uberaba'),
+('Juiz de Fora'),
+('Petrolina'),
+('Mogi das Cruzes'),
+('Marília'),
+('Bauru'),
+('Serrana'),
+('Palmas'),
+('Varginha'),
+('Itapecerica da Serra'),
+('Tatuí'),
+('São João Nepomuceno'),
+('Arujá'),
+('São Vicente'),
+('Montes Claros'),
+('Barra Mansa'),
+('Guarujá'),
+('Cabo Frio'),
+('Santos Dumont'),
+('Laguna'),
+('Araçatuba'),
+('São Sebastião'),
+('Ubá'),
+('Cascavel'),
+('Itaúna'),
+('Ribeirão das Neves'),
+('Andradina'),
+('Colatina'),
+('Sorriso'),
+('Maracanaú'),
+('Rio Branco'),
+('Barretos'),
+('Catanduva'),
+('Guaratinguetá'),
+('Dourados'),
+('São João da Boa Vista'),
+('Guapimirim'),
+('Barra do Garças'),
+('Araraquara'),
+('Assis'),
+('Bragança Paulista');
+
+create table public.especializacao (
+    nome character varying(200) not null,
+    constraint espicializacao_pkey primary key (nome)
+); 
+INSERT INTO public.especializacao (nome) VALUES
+('Artista Digital'),
+('Artista de Aquarela'),
+('Artista de Papel'),
+('Ilustrador de Quadrinhos'),
+('Ilustrador de Animes'),
+('Ilustrador de Cartoons');
--- src/index.ts
+++ src/index.ts
@@ -12,51 +12,123 @@ const supabaseUrl = 'https://ehqpuutchlhuztwcmjwb.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVocXB1dXRjaGxodXp0d2NtandiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMjA0NDU4MSwiZXhwIjoyMDM3NjIwNTgxfQ.1HEaNhn93f2t2Dv93XITUA0uPYpIOsAO-r8nOa0ZZC0';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-app.get('/api/test', (req: Request, res: Response) => {
-    res.send('Backend está funcionando');
-});
-
 // Rota para cadastrar um novo usuário
 app.post('/api/usuario', async (req: Request, res: Response) => {
-    const { nome, tipo, localizacao, descricao, zap, insta, face, twitter, foto, Senha, Email } = req.body;
-  
-    try {
-      // Inserir dados no Supabase
-      const { error  } = await supabase
-        .from('usuario')  // Nome da tabela no Supabase
-        .insert(
-          {
-            nome: nome,
-            tipo: tipo,
-            localizacao: localizacao,
-            descricao: descricao,
-            zap: zap,
-            insta: insta,
-            face: face,
-            twitter: twitter,
-            foto: foto,
-            Senha: Senha,
-            Email: Email,
-          }
-        );
-  
-      if (error) {
-        throw new Error(error.message);
+  const { nome, tipo, localizacao, descricao, zap, insta, face, twitter, foto, Senha, Email } = req.body;
+
+  try {
+    const { data, error } = await supabase
+      .from('usuario')
+      .insert(
+        {
+          nome: nome,
+          tipo: tipo,
+          localizacao: localizacao,
+          descricao: descricao,
+          zap: zap,
+          insta: insta,
+          face: face,
+          twitter: twitter,
+          foto: foto,
+          Senha: Senha,
+          Email: Email,
+        }
+      );
+    if (error) {
+      console.log(error.message);
+      let msgErro = error.message.toString().toLowerCase();
+      if (msgErro.includes('null value in column "senha"')) {
+        msgErro = 'O campo de senha esta vazia, preencha sua senha.';
       }
-      console.log(error);
-      if (!error) {
-        console.log('Dados inseridos com sucesso');
-        res.status(201).json(req.body);
-      } else {
-        console.log('400');
-        res.status(400).json({ error: error });
+      else if (msgErro.includes('null value in column "nome"')) {
+        msgErro = 'O campo de usuario esta vazio, preencha o seu nome de usuario.';
       }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Erro ao cadastrar usuário' });
+      else if (msgErro.includes('null value in column "email"')) {
+        msgErro = 'O campo de email esta vazio, preencha seu email.';
+      }
+      else if (msgErro.includes('duplicate key value')) {
+        msgErro = 'E-mail já cadastrado. Por favor, use um e-mail diferente.';
+      }
+
+      return res.status(400).json({ error: msgErro });
     }
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-  });
+
+    if (data) console.log('Dados inseridos com sucesso:', data);
+    if (data) res.status(201).json({ data: data[0] });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error });
+  }
+});
+
+// Rota para obter cidades
+app.get('/api/paises', async (req: Request, res: Response) => {
+  try {
+    const { data, error } = await supabase
+      .from('pais')
+      .select('nome');
+
+    if (error) {
+      throw new Error(error.message);
+    }
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao obter paises' });
+  }
+});
+
+// Rota para obter cidades
+app.get('/api/estados', async (req: Request, res: Response) => {
+  try {
+    const { data, error } = await supabase
+      .from('estado')
+      .select('nome');
+
+    if (error) {
+      throw new Error(error.message);
+    }
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao obter estados' });
+  }
+});
+
+// Rota para obter cidades
+app.get('/api/cidades', async (req: Request, res: Response) => {
+  try {
+    const { data, error } = await supabase
+      .from('cidade') 
+      .select('nome');
+
+    if (error) {
+      throw new Error(error.message);
+    }
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao obter cidades' });
+  }
+});
+
+// Rota para obter especializações
+app.get('/api/especializacoes', async (req: Request, res: Response) => {
+  try {
+    const { data, error } = await supabase
+      .from('especializacao') 
+      .select('nome');
+
+    if (error) {
+      throw new Error(error.message);
+    }
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao obter especializações' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
