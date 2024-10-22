e0e02bd26bce3312b8add09983500772bfcce829
Th14g0o <geekland135@gmail.com>
2024-10-21 21:03:34 -0300

    porta

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
