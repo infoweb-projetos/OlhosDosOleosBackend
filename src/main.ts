9c0e09a5d3276511dac8a87c4be72d831e785033
Th14g0o <geekland135@gmail.com>
2024-10-21 21:16:38 -0300

    port e host

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
@@ -14,6 +15,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(port, "0.0.0.0");
 }
 bootstrap();
